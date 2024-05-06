11be3e665eefa2be3c1d69571352281d92339c2d
Deepak Thakur <dt9971678995@gmail.com>
2024-05-06 23:30:15 +0530

    added helper.py file

---
+++ src/helper.py
@@ -0,0 +1,43 @@
+import os
+from git import Repo # with Repo we can clone any git repository
+from langchain.text_splitter import Language #to perform context aware splitting
+from langchain.document_loaders.generic import GenericLoader # to load the github repo(codebase)
+from langchain.document_loaders.parsers import LanguageParser #LanguageParser will identify the programming language
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+
+
+#clone any github repository
+def repo_ingestion(repo_url):
+    os.makedirs("repo", exist_ok=True)
+    repo_path = "repo/"
+    Repo.clone_from(repo_url, to_path=repo_path)
+
+
+# Loading repositories as documents
+def load_repo(repo_path):
+    loader = GenericLoader.from_filesystem(repo_path,
+                                           glob = "**/*",
+                                           suffixes=[".py"],
+                                           parser = LanguageParser(language=Language.PYTHON, parser_threshold=500))
+    
+    documents = loader.load()
+    return documents
+
+
+#Creating text chunks 
+def text_splitter(documents):
+    documents_splitter = RecursiveCharacterTextSplitter.from_language(language = Language.PYTHON,
+                                                             chunk_size = 2000,
+                                                             chunk_overlap = 200)
+    
+    text_chunks = documents_splitter.split_documents(documents)
+
+    return text_chunks
+
+#loading embeddings model
+def load_embedding():
+    embeddings=HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',                                  
+                                 model_kwargs={'device':'cpu'})
+    return embeddings
